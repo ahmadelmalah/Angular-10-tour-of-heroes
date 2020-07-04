dadce703b6bbc39812c53394d8c251059ad21d1c
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2020-07-04 16:29:56 +0200

    Displaying a basic version of compenent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
 
 @Component({
   selector: 'app-heroes',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  hero = 'Windstorm';
+  
   constructor() { }
 
   ngOnInit(): void {
